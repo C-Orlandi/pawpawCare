85f6fe96a622cd3ecc172b0ece41f9ad9cbe7026
Cristobal Orellana <orlandi.alenn@gmail.com>
2025-07-01 18:31:01 -0400

    nuevo

--- pawbackend/index.js
+++ pawbackend/index.js
@@ -11,13 +11,7 @@ console.log('API Key:', process.env.GOOGLE_MAPS_API_KEY);
 const app = express();
 
 // Middleware global — debe ir primero para habilitar CORS y parsear JSON
-app.use(cors({
-  origin: ['http://localhost:8100', 'https://pawpawcare.onrender.com'], // aquí tus orígenes frontend válidos
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
-
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
